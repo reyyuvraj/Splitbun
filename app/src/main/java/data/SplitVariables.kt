3d6ab6845d35e9971a4bbd4d627f615ef0699742
reyyuvraj <yuvrajsingh77733999@gmail.com>
2021-04-29 21:16:03 +0530

    Initial commit

---
+++ app/src/main/java/data/SplitVariables.kt
@@ -0,0 +1,8 @@
+package data
+
+data class SplitVariables(
+        val name: String = "515495",
+        val cost: Int = 13000000 ,
+        val material: String = "rent",
+        val split: Boolean = false
+)
